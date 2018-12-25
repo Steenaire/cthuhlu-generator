7d502dedbb2706d5db1663aead77cf24b7609f7c
Steenaire <cmclarkey@gmail.com>
2018-12-24 17:32:21 -0800

    Make random price and calorie counts for menu items

---
+++ cthuhlu-generator.rb
@@ -0,0 +1,21 @@
+# require Random
+
+words_hash = {}
+line_words_array = []
+all_words_array = []
+
+words_popularity_hash = {}
+
+probabilities_hash = {}
+
+wordcounts = []
+
+randomizer_words_array = []
+opening_words_array = []
+
+calories = Random.rand(2000)
+price = Random.rand(100)+Random.rand.round(2)
+
+puts "Price: $#{price}"
+puts "Calories: #{calories}"
+
